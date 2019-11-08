e418073f0e213961ab52ab8fd50ee7c196096cdb
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2019-11-08 17:17:17 -0300

    Atualizar arquivo ga.c

--- ga.c
+++ ga.c
@@ -7,12 +7,17 @@ void main() {
 	int i, j, size, n_gens, **pop;
 	size = 4;
 	n_gens = 8;
-	for (i = 0; i < POP_SIZE; i++) {
-		for (j = 0; j < NUM_GENS; j++) {
+	pop = malloc(size * sizeof(int*));
+	for (i = 0; i < size; i++) {
+		pop[i] = malloc(n_gens * sizeof(int));
+		for (j = 0; j < n_gens; j++) {
 			pop[i][j] = 0;
 		}
 	}
 	print(pop, size, n_gens);
+	for (i = 0; i < size; i++)
+		free(pop[i]);
+	free(pop);
 }
 
 void print(int *pop, int size, int n_gens) {
