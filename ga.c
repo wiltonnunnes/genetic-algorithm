7ce27c9b7d4042031d6413c61650322eac17eaff
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2019-11-11 10:47:05 -0300

    Atualizar ga.c

--- ga.c
+++ ga.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "random.h"
 
-void print(int *pop, int size, int n_gens);
+void print(int **pop, int size, int n_gens);
 
 void main() {
 	int i, j, size, n_gens, **pop;
 	size = 4;
 	n_gens = 8;
 	pop = malloc(size * sizeof(int*));
+	initSeed();
 	for (i = 0; i < size; i++) {
 		pop[i] = malloc(n_gens * sizeof(int));
 		for (j = 0; j < n_gens; j++) {
-			pop[i][j] = 0;
+			pop[i][j] = randomInteger(0, 2);
 		}
 	}
 	print(pop, size, n_gens);
@@ -20,11 +22,11 @@ void main() {
 	free(pop);
 }
 
-void print(int *pop, int size, int n_gens) {
+void print(int **pop, int size, int n_gens) {
 	int i, j;
 	for (i = 0; i < size; i++) {
 		for (j = 0; j < n_gens; j++) {
-			printf("%u", *(pop + i * n_gens + j));
+			printf("%u", pop[i][j]);
 		}
 		printf("\n");
 	}
